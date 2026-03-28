efc0b8de454fd1573f53064aaa99c28f5cb92961
Samuel Clay <samuel@ofbrooklyn.com>
2026-03-27 17:32:52 -0700

    Fix Android Daily Briefing reader icon styles

--- clients/android/NewsBlur/app/src/main/assets/storyDetailView.js
+++ clients/android/NewsBlur/app/src/main/assets/storyDetailView.js
@@ -5,12 +5,48 @@ function loadImages() {
     }
 }
 
+function hasClass(img, className) {
+    return !!img.classList && img.classList.contains(className);
+}
+
+function hasProtectedImageClass(img) {
+    return hasClass(img, 'NB-briefing-inline-favicon') ||
+        hasClass(img, 'NB-briefing-section-icon') ||
+        hasClass(img, 'NB-classifier-icon-like') ||
+        hasClass(img, 'NB-classifier-icon-dislike') ||
+        hasClass(img, 'NB-classifier-icon-dislike-inner');
+}
+
+function setImageClass(img, className) {
+    if (img.classList) {
+        img.classList.remove('NB-large-image');
+        img.classList.remove('NB-small-image');
+        img.classList.add(className);
+        return;
+    }
+
+    var classAttr = img.getAttribute('class') || '';
+    var classNames = classAttr.split(/\s+/);
+    var filtered = [];
+    for (var i = 0, len = classNames.length; i < len; i++) {
+        if (classNames[i] && classNames[i] !== 'NB-large-image' && classNames[i] !== 'NB-small-image') {
+            filtered.push(classNames[i]);
+        }
+    }
+    filtered.push(className);
+    img.setAttribute('class', filtered.join(' '));
+}
+
 function setImage(img) {
+    if (hasProtectedImageClass(img)) {
+        return;
+    }
+
     if (img.querySelector('tagName') == 'VIDEO') {
-        img.setAttribute('class', 'NB-large-image');
+        setImageClass(img, 'NB-large-image');
     } else if (img.width >= 320 && img.height >= 50) {
-        img.setAttribute('class', 'NB-large-image');
+        setImageClass(img, 'NB-large-image');
     } else {
-        img.setAttribute('class', 'NB-small-image');
+        setImageClass(img, 'NB-small-image');
     }
 }
